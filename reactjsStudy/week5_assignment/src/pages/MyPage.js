988298844a13ac0954b6b8b52da7765d1bf0b0dc
이소정 <66251759+i-soj-ng@users.noreply.github.com>
2021-01-04 03:30:50 +0900

    Add files via upload

---
+++ reactjsStudy/week5_assignment/src/pages/MyPage.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './mypage.css';
+
+export function Mypage() {
+    return <>
+        <div className="entire">
+            <h1 id='myinfo'>My Info</h1>
+            <div className="info">
+                <p id='name'>Name: 이소정</p>
+                <p id='birth'>Birth: 2001.02.16</p>
+                <p id='school'>School: Kookmin Univ.</p>
+                <p id='insta'>Instagram: isojng</p>
+                <p id='etc'>Please follow me ^__^</p>
+            </div>
+        </div>
+    </>;
+}
